742b494820189ed500e24ceb1729c838bbfd607d
Aline Soares <aline01600@gmail.com>
2025-08-01 10:08:30 -0300

    Adicionando campo texto e corrigindo erros

---
+++ lib/campo_texto.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CampoTexto extends StatefulWidget {
+  const CampoTexto({super.key});
+
+  @override
+  State<CampoTexto> createState() => _CampoTextoState();
+}
+
+class _CampoTextoState extends State<CampoTexto> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Entrada de dados"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(), // ou outro widget
+          ),
+        ],
+      ),
+    );
+  }
+}
